566b9ee754d51e546fce439ca05eda7e5cc6c381
JASPER-JESSY <jasperjc1910@gmail.com>
2026-07-06 22:00:54 +0530

    Add files via upload
    Implemented Singleton Design Pattern with Logger and Main classes

---
+++ SingletonPatternExample/src/Main.java
@@ -0,0 +1,18 @@
+
+public class Main {
+    public static void main(String[] args) {
+        
+        Logger logger1 = Logger.getInstance();
+        logger1.log("First message");
+        
+        Logger logger2 = Logger.getInstance();
+        logger2.log("Second message");
+        
+        if (logger1 == logger2) {
+            System.out.println("Both logger1 and logger2 are the same instance");
+        }
+        
+        System.out.println("logger1 hashCode: " + logger1.hashCode());
+        System.out.println("logger2 hashCode: " + logger2.hashCode());
+    }
+}
