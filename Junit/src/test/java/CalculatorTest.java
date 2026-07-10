f04b902c86754da5728df676681aafd0f312279c
JASPER-JESSY <jasperjc1910@gmail.com>
2026-07-10 23:10:46 +0530

    Add files via upload
    My Exercise on JUnit Testing at First in Setting Up

---
+++ Junit/src/test/java/CalculatorTest.java
@@ -0,0 +1,30 @@
+package com.example;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CalculatorTest {
+
+    Calculator calc = new Calculator();
+
+    @Test
+    public void testAddition() {
+        assertEquals(5, calc.add(2,3));
+    }
+
+    @Test
+    public void testSubtraction() {
+        assertEquals(3, calc.subtract(5,2));
+    }
+
+    @Test
+    public void testMultiplication() {
+        assertEquals(12, calc.multiply(3,4));
+    }
+
+    @Test
+    public void testDivision() {
+        assertEquals(2, calc.divide(10,5));
+    }
+}
